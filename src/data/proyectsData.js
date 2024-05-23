84b29acc4929e75b2ab01677809cfba15a42cdc3
Santiago N.S. <gigogisantisd@gmail.com>
2024-05-22 23:55:06 -0300

    Create proyectsData.js

---
+++ src/data/proyectsData.js
@@ -0,0 +1,60 @@
+import filterAppImg from "../img/proyects/filter-users-app.png"
+import calculatorImg from "../img/proyects/calculator.png"
+import taskListImg from "../img/proyects/taskslist.jpg"
+import piBeerImg from "../img/proyects/pibeer.png"
+import portfolioImg from "../img/proyects/portfolio.png"
+
+const proyects = [
+    {
+        title:"Filter Users App",
+        hasHTML:true,
+        hasCss:true,
+        hasJavaScript:true,
+        hasReact:true,
+        hasBootstrap:true,
+        proyectImg:filterAppImg,
+        urlGitHub:"https://github.com/SantiagoNunezSelem/Filter-Users-App",
+        urlProyect:"https://santiagonunezselem.github.io/Filter-Users-App/"
+    },
+    {
+        title:"Calculator",
+        hasHTML:true,
+        hasCss:true,
+        hasJavaScript:true,
+        hasReact:true,
+        hasBootstrap:true,
+        proyectImg:calculatorImg,
+        urlGitHub:"https://github.com/SantiagoNunezSelem/Web-Calculator",
+        urlProyect:"https://santiagonunezselem.github.io/Web-Calculator/"
+    },
+    {
+        title:"Tasks List",
+        hasHTML:true,
+        hasCss:true,
+        hasJavaScript:true,
+        hasReact:true,
+        proyectImg:taskListImg,
+        urlGitHub:"https://github.com/SantiagoNunezSelem/Tasks-List",
+        urlProyect:"https://santiagonunezselem.github.io/Tasks-List/"
+    },
+    {
+        title:"PiBeer",
+        hasHTML:true,
+        hasCss:true,
+        proyectImg:piBeerImg,
+        urlGitHub:"https://github.com/SantiagoNunezSelem/piBeer",
+        urlProyect:"https://santiagonunezselem.github.io/piBeer"
+    },
+    {
+        title:"Portfolio",
+        hasHTML:true,
+        hasCss:true,
+        hasJavaScript:true,
+        hasReact:true,
+        proyectImg:portfolioImg,
+        urlGitHub:"https://github.com/SantiagoNunezSelem/Portfolio",
+        urlProyect:"#home-section"
+    },
+]
+
+export default proyects
