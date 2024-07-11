3c945789d86785f23c034192bad7e5cad19a6931
Santiago N.S <gigogisantisd@gmail.com>
2024-07-11 00:40:03 -0300

    Add skill C# and .NET Framework

--- src/components/AboutMe/SkillList.js
+++ src/components/AboutMe/SkillList.js
@@ -4,8 +4,8 @@ import htmlImg from "../../img/symbols/html5-symbol.png";
 import cssImg from "../../img/symbols/css-symbol.png";
 import javaScriptImg from "../../img/symbols/javascript-symbol.png";
 import reactImg from "../../img/symbols/react-symbol.png";
-import cPlusPlusImg from "../../img/symbols/cPlusPlus-symbol.png";
-import pythonImg from "../../img/symbols/python-symbol.png";
+import netFramework from "../../img/symbols/net-framework-symbol.png";
+import cSharp from "../../img/symbols/cSharp-symbol.png";
 import enterpriseArchitectImg from "../../img/symbols/enterprise-architect-symbol.png";
 import figmaImg from "../../img/symbols/figma-symbol.png";
 import trelloImg from "../../img/symbols/trello-symbol.png";
@@ -20,8 +20,8 @@ function SkillList(){
                     <Skill title="CSS" img={cssImg}/>
                     <Skill title="JavaScript" img={javaScriptImg}/>
                     <Skill title="React" img={reactImg}/>
-                    <Skill title="C++" img={cPlusPlusImg}/>
-                    <Skill title="Python" img={pythonImg}/>
+                    <Skill title=".NET Framework" img={netFramework}/>
+                    <Skill title="C Sharp" img={cSharp}/>
                 </div>
             </div>
 
