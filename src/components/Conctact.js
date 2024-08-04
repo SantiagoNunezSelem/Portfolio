586be75d4b932bed95cbfd3834105d5b0bd584e0
Santiago N.S. <gigogisantisd@gmail.com>
2024-08-04 18:55:25 -0300

    Add my youtube channel

--- src/components/Conctact.js
+++ src/components/Conctact.js
@@ -1,7 +1,8 @@
-import "../stylesheets/Contact.css";
-import emailImg from "../img/symbols/email-symbol.png";
-import linkedInImg from "../img/symbols/linkedin-symbol.png";
-import gitHubImg from "../img/symbols/github-symbol.png";
+import "../stylesheets/Contact.css"
+import emailImg from "../img/symbols/email-symbol.png"
+import linkedInImg from "../img/symbols/linkedin-symbol.png"
+import gitHubImg from "../img/symbols/github-symbol.png"
+import youtube from "../img/symbols/youtube-symbol.png"
 
 
 function Contact(){
@@ -15,6 +16,7 @@ function Contact(){
                 <a href="mailto:selemsantiago@gmail.com" target="_blank"><img src={emailImg}/></a>
                 <a href="https://www.linkedin.com/in/santiago-nu%C3%B1ez-selem-845000243/" target="_blank"><img src={linkedInImg}/></a>
                 <a href="https://github.com/SantiagoNunezSelem" target="_blank"><img src={gitHubImg}/></a>
+                <a href="https://www.youtube.com/channel/UCndbqjiPZonSJ8-jSe85gGg" target="_blank"><img src={youtube}/></a>
             </div>
         </div>
     );
