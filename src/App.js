b46ea0fdb12b7f87c65a32fe35690334124b6120
Santiago N.S. <gigogisantisd@gmail.com>
2024-08-06 11:11:09 -0300

    Advertencia del navegador samsung con tema oscuro
    El navegador de samsung es el navegador default. Cuando el modo oscuro esta activado cambia los colores de las paginas web.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import video from "./video/video.mp4";
 import Contact from "./components/Conctact.js";
 
 function App() {
+  if(navigator.userAgent.match(/samsung/i)){
+    alert("El modo oscuro del navegador que está utilizando puede no mostrar correctamente este sito web. "
+        + "Le recomendamos usar otro navegador como Firefox, Microsoft Edge o Google Chrome.")
+  }
+
   return (
     <div className="App">
 
