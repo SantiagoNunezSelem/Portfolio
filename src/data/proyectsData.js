e1b0d9c0e9525575ccbb9b584943544d20194fe0
Santiago N.S. <gigogisantisd@gmail.com>
2024-12-25 13:20:35 -0300

    adding projects

--- src/data/proyectsData.js
+++ src/data/proyectsData.js
@@ -6,22 +6,40 @@ import bootstrapSymbol from "../img/symbols/bootstrap-symbol.png"
 import tailwindSymbol from "../img/symbols/tailwind-symbol.png"
 import cSharp from "../img/symbols/cSharp-symbol.png"
 import netFramework from "../img/symbols/net-framework-symbol.png"
+import microsoftSqlServer from "../img/symbols/microsoft-sql-server-symbol.png"
+import azure from "../img/symbols/azure-development.png"
+import entityFramework from "../img/symbols/entity-framework-symbol.png"
 
+import infractionSystem from "../img/proyects/infractionSystem.png"
 import sportClub from "../img/proyects/sport-club.jpg"
 import barberShop from "../img/proyects/barberShop.png"
+import GameOfLife from "../img/proyects/gameOfLife.png"
 import filterAppImg from "../img/proyects/filter-users-app.png"
 import calculatorImg from "../img/proyects/calculator.png"
 import taskListImg from "../img/proyects/taskslist.jpg"
 import piBeerImg from "../img/proyects/pibeer.png"
 import portfolioImg from "../img/proyects/portfolio.png"
 
-
 const proyects = [
+    {
+        title:"Infraction System",
+        technologies:{
+            cSharp: {symbol: cSharp, name:"C#", className:""},
+            netFramework: {symbol: netFramework, name:".NET Framework", className: "circle-symbol"},
+            microsoftSqlServer: {symbol: microsoftSqlServer, name:"Microsoft SQL Server", className: "circle-symbol"},
+            entityFramework: {symbol: entityFramework, name:"Entity Framework", className: "circle-symbol"},
+            azure: {symbol: azure, name:"Microsoft Azure", className: "circle-symbol"}
+        },
+        proyectImg:infractionSystem,
+        urlVideo:"",
+        urlGitHub:""
+    },
+    
     {
         title:"Sport Club",
         technologies:{
             cSharp: {symbol: cSharp, name:"C#", className:""},
-            netFramework: {symbol: netFramework, name:".NET Framework", className: "netFramework"},
+            netFramework: {symbol: netFramework, name:".NET Framework", className: "circle-symbol"},
         },
         proyectImg:sportClub,
         urlVideo:"https://youtu.be/Tg1rY32__eo",
@@ -40,6 +58,19 @@ const proyects = [
         proyectImg:barberShop,
         urlGitHub:"https://github.com/SantiagoNunezSelem/No-Country-C17/tree/main-sin-db",
         urlProyect:"https://c17-peluqueria.vercel.app/"
+    },
+
+    {
+        title:"Game of Life",
+        technologies:{
+            html: { symbol: htmlSymbol,name: "HTML", className: "" },
+            css: { symbol: cssSymbol,name: "CSS", className: "" },
+            javascript: { symbol: javaScriptSymbol,name: "JavaScript", className: "" },
+            react: { symbol: reactSymbol, name: "React", className: "reactSymbol" }
+        },
+        proyectImg:GameOfLife,
+        urlGitHub:"https://github.com/SantiagoNunezSelem/GameOfLife",
+        urlProyect:"https://santiagonunezselem.github.io/GameOfLife"
     },
 
     {
